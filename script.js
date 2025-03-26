15a0b33a85162e2d852d8b141b9011affbf9a22b
faz0189 <128984518+faz0189@users.noreply.github.com>
2025-03-26 18:11:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,7 +6,5 @@ function indexOfIgnoreCase(str, subStr) {
 console.log(indexOfIgnoreCase("Hello World", "world")); // 6
 console.log(indexOfIgnoreCase("apple", "Ple")); // 2
 console.log(indexOfIgnoreCase("test", "aaa")); // -1
-console.log(indexOfIgnoreCase("JavaScript", "script")); // 4
-console.log(indexOfIgnoreCase("", "test")); // -1
-console.log(indexOfIgnoreCase("test", "")); // 0
+
 
