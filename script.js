0d23e96ff1a5818b356b252b8a985993f038e5b8
faz0189 <128984518+faz0189@users.noreply.github.com>
2025-03-26 18:06:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-function indexOfIgnoreCase(s1, s2) {
-  // write your code here
+function indexOfIgnoreCase(str, subStr) {
+    return str.toLowerCase().indexOf(subStr.toLowerCase());
 }
 
-// Please do not change the code below
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(indexOfIgnoreCase(s1, s2));
+// Test cases
+console.log(indexOfIgnoreCase("Hello World", "world")); // 6
+console.log(indexOfIgnoreCase("apple", "Ple")); // 2
+console.log(indexOfIgnoreCase("test", "aaa")); // -1
+console.log(indexOfIgnoreCase("JavaScript", "script")); // 4
+console.log(indexOfIgnoreCase("", "test")); // -1
+console.log(indexOfIgnoreCase("test", "")); // 0
+
