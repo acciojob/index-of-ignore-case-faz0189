f74f5308f6186c646e349227751b6abfbec982bc
faz0189 <128984518+faz0189@users.noreply.github.com>
2025-03-26 19:12:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-function indexOfIgnoreCase(str, subStr) {
-    return str.toLowerCase().indexOf(subStr.toLowerCase());
+function indexOfIgnoreCase(str:string, subStr: string){
+	return str.toLowerCase().indexOf(subStr.toLowerCase())
 }
 
-// Test cases
-console.log(indexOfIgnoreCase("Hello World", "world")); // 6
-console.log(indexOfIgnoreCase("apple", "Ple")); // 2
-console.log(indexOfIgnoreCase("test", "aaa")); // -1
+console.log(indexOfIgnoreCase("Hello Wordld","world"))
+console.log(indexOfIgnoreCase("kev is ","Is"))
+
 
 
